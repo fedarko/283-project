636b4e57158d4cf936c2c77c2b66804724f7e424
Marcus Fedarko <mfedarko@umd.edu>
2021-06-02 22:36:40 -0700

    upd8 sparsity script: just cancer matrix

--- scripts/sparsity.py
+++ scripts/sparsity.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python3
-# Quickly computes the sparsity of the total matrix, just so we can mention
+# Quickly computes the sparsity of the cancer matrix, just so we can mention
 # this in the slides.
 
 import pandas as pd
-df = pd.read_csv("data/GSE131512_cancerTPM.txt", sep="\t", index_col=0)
-df2 = pd.read_csv("data/GSE131512_normalTPM.txt", sep="\t", index_col=0)
-# Merge matrices
-mat = pd.concat([df, df2], axis=1)
+mat = pd.read_csv("data/GSE131512_cancerTPM.txt", sep="\t", index_col=0)
 
 # Compute sparsity
 sparsity = (mat == 0).sum().sum() / (len(mat.columns) * len(mat.index))
